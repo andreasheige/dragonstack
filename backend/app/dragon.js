39cfaeea512526f0d4fcb15274ea6cbe1538a6e3
Andreas Heige <a.heige.83@gmail.com>
2020-04-17 21:33:41 +0200

    organization

---
+++ backend/app/dragon.js
@@ -0,0 +1,34 @@
+const TRAITS = require('../traits.json')
+
+const DEFAULT_PROPERTIES = {
+    nickname: 'unnamed',
+    get birthdate() {
+        return new Date()
+    },
+    get randomTraits(){
+        const traits = [];
+
+        TRAITS.forEach(TRAITS => {
+           const traitType = TRAITS.type;
+           const traitsValues = TRAITS.values;
+           
+           const traitsValue = traitsValues[
+               Math.floor(Math.random() * traitsValues.length)
+            ];
+
+            traits.push({ traitType, traitsValue });
+        });
+
+        return traits;
+    }
+}
+
+class Dragon {
+    constructor({ birthdate, nickname, traits } = {}) {
+        this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
+        this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
+        this.traits = traits || DEFAULT_PROPERTIES.randomTraits;
+    }
+}
+
+module.exports = Dragon;
---
+++ backend/app/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const GenerationEngine = require('./generation/engine');
+
+const app = express();
+const engine = new GenerationEngine();
+const port = 3000;
+
+engine.start();
+
+app.get('/dragon/new', (req, res) => {
+    res.json({ dragon: engine.generation.newDragon() });
+});
+
+app.listen(port, () => console.log(`listening on port ${port}`));
+
+module.exports = app;
