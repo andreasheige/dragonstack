88a9430f1b044ea2de569ec733b37279485565bf
Andreas Heige <a.heige.83@gmail.com>
2020-04-18 20:33:07 +0200

    ...

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import Generation from './components/Generation';
 
 render(
-    <div>Dragonstack from React YO!</div>,
+    <div>
+        <h2>Dragonstack from React YO!</h2>
+        <Generation />
+    </div>,
     document.getElementById('root')
 )
