8b9a319148518300be7b5fd48cf76d6ac34cbe57
Andreas Heige <a.heige.83@gmail.com>
2020-05-08 14:02:20 +0200

    component added

--- frontend/src/components/Generation.js
+++ frontend/src/components/Generation.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 
 class Generation extends Component {
+  state = { generation: { generationId: 999, expiration: '2020-02-20' }};
+
+  componentDidMount() {
+      this.fetchGeneration();
+  }
+
+  fetchGeneration = () => {
+      fetch('http://localhost:3000/generation')
+      .then(response => console.log('response', response));
+  };
+
     render() {
-        const generation = { generationId: 999, expiration: '2020-02-20' };
+        const { generation } = this.state;
+
         return (
             <div>
                 <h3>Generation {generation.generationId}. Expires on:</h3>
